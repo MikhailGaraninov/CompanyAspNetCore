8f1a0e786cf2643cec521c2b798e3f7d0e5684da
MikhailGaraninov <78325389+MikhailGaraninov@users.noreply.github.com>
2022-07-17 21:03:12 +0300

    Готова панель администратора, работают функции добавления и редактирования услуг и текстовых полей, нету ссылок и views

---
+++ CompanyAspNetCore/Areas/Admin/Controllers/ServiceItemsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using CompanyAspNetCore.Domain;
+using CompanyAspNetCore.Domain.Entities;
+using CompanyAspNetCore.Service;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CompanyAspNetCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceItemsController : Controller
+    {
+        private readonly DataManager dataManager;
+        private readonly IWebHostEnvironment hostingEnvironment;
+        public ServiceItemsController(DataManager dataManager, IWebHostEnvironment hostingEnvironment)
+        {
+            this.dataManager = dataManager;
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            var entity = id == default ? new ServiceItem() : dataManager.ServiceItems.GetServiceItemById(id);
+            return View(entity);
+        }
+        [HttpPost]
+        public IActionResult Edit(ServiceItem model, IFormFile titleImageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (titleImageFile != null)//если титульная картинка не равна нулю, берем название картинки из файла
+                {
+                    model.TitleImagePath = titleImageFile.FileName;
+                    using (var stream = new FileStream(Path.Combine(hostingEnvironment.WebRootPath, "images/", titleImageFile.FileName), FileMode.Create))//титульная картинка после добавления попадает в wwwroot/images
+                    {
+                        titleImageFile.CopyTo(stream);
+                    }
+                }
+                dataManager.ServiceItems.SaveServiceItem(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Guid id)
+        {
+            dataManager.ServiceItems.DeleteServiceItem(id);
+            return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+        }
+    }
+}
